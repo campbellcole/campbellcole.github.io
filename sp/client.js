2a7295c68e1fdffe591e794263e62fff8d2010fa
Campbell Cole <cmcoleco@gmail.com>
2020-03-13 16:00:11 -0700

    sp

--- sp/client.js
+++ sp/client.js
@@ -1,6 +1,6 @@
 M.AutoInit();
 
-const SPAWNRATE = 5000;
+const SPAWNRATE = 400; // bigger is less frequently
 
 var canv = document.querySelector('canvas');
 var ctx = canv.getContext('2d');
@@ -12,9 +12,19 @@ function particle() {
     ctx.beginPath();
     ctx.ellipse(Math.random() * canv.width, Math.random() * canv.height, 1, 1, 0, 0, 2 * Math.PI, false);
     ctx.stroke();
+    ctx.fill();
     setTimeout(() => { particle(); }, Math.random() * SPAWNRATE);
 }
 
-ctx.strokeStyle = "#F0F0F0";
+var cgrad = ctx.createLinearGradient(0, canv.height/2, canv.width, canv.height/2);
+cgrad.addColorStop('0', 'black');
+cgrad.addColorStop('0.4', 'grey');
+cgrad.addColorStop('0.6', 'grey');
+cgrad.addColorStop('1', 'black');
 
-particle();
+ctx.strokeStyle = cgrad;
+ctx.fillStyle = cgrad;
+ctx.globalAlpha = 0.6;
+ctx.lineWidth = 0.5;
+
+//particle();
